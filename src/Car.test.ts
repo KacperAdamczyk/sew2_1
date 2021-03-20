14fda69beb0405ae8f93bfc3fdb0b6a316e36df2
Kacper Adamczyk <kacper.k.adamczyk@gmail.com>
2021-03-20 18:17:26 +0100

    Car drive - fail

--- src/Car.test.ts
+++ src/Car.test.ts
@@ -32,4 +32,47 @@ describe('Car', () => {
       new Error('You cannot refuel with a negative value!'),
     );
   });
+
+  describe('Car - drive', () => {
+    beforeEach(() => {
+      car.refuel(50);
+    });
+
+    it('should drive a given distance', () => {
+      const distance = car.drive(2);
+
+      expect(distance).toEqual(2);
+      expect(car.FuelLevel).toBeCloseTo(50 - 2 * 6.5);
+      expect(car.Odometer).toEqual(2);
+      expect(car.DailyOdometer).toEqual(2);
+    });
+
+    it('should drive max distance', () => {
+      const distance = car.drive(10);
+
+      const maxDistance = 50 / 6.5;
+      expect(distance).toBeCloseTo(maxDistance);
+      expect(car.FuelLevel).toEqual(0);
+      expect(car.Odometer).toBeCloseTo(maxDistance);
+      expect(car.DailyOdometer).toBeCloseTo(maxDistance);
+    });
+
+    it('should overflow odometers', () => {
+      let distance = car.drive(10);
+      car.refuel(50);
+      distance += car.drive(10);
+      car.refuel(50);
+      distance += car.drive(10);
+
+      const maxDistanceOnOneTank = 50 / 6.5;
+      expect(distance).toBeCloseTo(maxDistanceOnOneTank * 3);
+      expect(car.FuelLevel).toEqual(0);
+      expect(car.Odometer).toBeCloseTo(
+        (maxDistanceOnOneTank * 3) % Car.ODOMETER_MAX,
+      );
+      expect(car.DailyOdometer).toBeCloseTo(
+        (maxDistanceOnOneTank * 3) % Car.DAILY_ODOMETER_MAX,
+      );
+    });
+  });
 });
