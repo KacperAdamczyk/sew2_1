44284df5104431a0c7d600bfb35620d1f70613aa
Kacper Adamczyk <kacper.k.adamczyk@gmail.com>
2021-03-20 17:40:27 +0100

    Car constructor - pass

--- src/Car.ts
+++ src/Car.ts
@@ -1,1 +1,21 @@
-export class Car {}
+export class Car {
+  constructor(
+    private color: string,
+    private make: string,
+    private fuelConsumption: number,
+    private tankCapacity: number,
+  ) {}
+
+  get Color() {
+    return this.color;
+  }
+  get Make() {
+    return this.make;
+  }
+  get FuelConsumption() {
+    return this.fuelConsumption;
+  }
+  get TankCapacity() {
+    return this.tankCapacity;
+  }
+}
