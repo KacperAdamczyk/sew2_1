f69939c8df0c84102d61cc0c30f6f997298be708
Kacper Adamczyk <kacper.k.adamczyk@gmail.com>
2021-03-20 17:40:53 +0100

    Car refuel - fail

--- src/Car.test.ts
+++ src/Car.test.ts
@@ -1,13 +1,35 @@
 import { Car } from './Car';
 
 describe('Car', () => {
-  it('should create a class instance', () => {
-    const car = new Car('red', 'Ford', 6.5, 50);
+  let car: Car;
 
+  beforeEach(() => {
+    car = new Car('red', 'Ford', 6.5, 50);
+  });
+
+  it('should create a class instance', () => {
     expect(car).toBeInstanceOf(Car);
     expect(car.Color).toEqual('red');
     expect(car.Make).toEqual('Ford');
     expect(car.FuelConsumption).toEqual(6.5);
     expect(car.TankCapacity).toEqual(50);
+  });
+
+  it('should partially refuel', () => {
+    car.refuel(25);
+
+    expect(car.Fuel).toEqual(25);
+  });
+
+  it('should refuel to the max value', () => {
+    car.refuel(100);
+
+    expect(car.Fuel).toEqual(50);
+  });
+
+  it('should throw when refueling with a negative value', () => {
+    expect(() => car.refuel(-10)).toThrowError(
+      new Error('You cannot refuel with a negative value!'),
+    );
   });
 });
